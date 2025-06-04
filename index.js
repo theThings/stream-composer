08480737aeb4b8586a838975f3037450c443e6ea
TTiOSuils <f.suils@thethings.io>
2025-06-04 11:56:14 +0200

    Fix script for compatible nodejs 4.7.0

--- index.js
+++ index.js
@@ -1,194 +1,161 @@
-const streamx = require('streamx');
-const Duplex = streamx.Duplex;
-const pipeline = streamx.pipeline;
+var stream = require('stream'); // En lugar de "streamx"
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 
-module.exports = class Composer extends Duplex {
-  constructor (opts) {
-    super(opts)
+// Constructor para la clase Composer
+function Composer(opts) {
+  stream.Duplex.call(this, opts);
+  
+  this._writable = null;
+  this._readable = null;
+  this._isPipeline = false;
+  this._pipelineMissing = 2;
+  this._writeCallback = null;
+  this._finalCallback = null;
 
-    this._writable = null
-    this._readable = null
-    this._isPipeline = false
-    this._pipelineMissing = 2
-
-    this._writeCallback = null
-    this._finalCallback = null
-
-    this._ondata = this._pushData.bind(this)
-    this._onend = this._pushEnd.bind(this, null)
-    this._ondrain = this._continueWrite.bind(this, null)
-    this._onfinish = this._maybeFinal.bind(this)
-    this._onerror = this.destroy.bind(this)
-    this._onclose = this.destroy.bind(this, null)
-  }
-
-  static pipeline (...streams) {
-    const c = new Composer()
-    c.setPipeline(...streams)
-    return c
-  }
-
-  static duplexer (ws = null, rs = null) {
-    const c = new Composer()
-    c.setWritable(ws)
-    c.setReadable(rs)
-    return c
-  }
-
-  setPipeline (first, ...streams) {
-    const all = Array.isArray(first) ? first : [first, ...streams]
-
-    this._isPipeline = true
-    this.setWritable(all[0])
-    this.setReadable(all[all.length - 1])
-
-    pipeline(all, (err) => {
-      if (err) this.destroy(err)
-    })
-
-    return this
-  }
-
-  setReadable (rs) {
-    if (this._readable) {
-      this._readable.removeListener('data', this._ondata)
-      this._readable.removeListener('end', this._onend)
-      this._readable.removeListener('error', this._onerror)
-      this._readable.removeListener('close', this._onclose)
-    }
-
-    if (rs === null) {
-      this._readable = null
-      this.push(null)
-      this.resume()
-      return
-    }
-
-    this._readable = rs
-    this._readable.on('data', this._ondata)
-    this._readable.on('end', this._onend)
-    this._readable.on('error', this._onerror)
-    this._readable.on('close', this._onclose)
-
-    if (this.destroying && this._readable.destroy) {
-      this._readable.destroy()
-    }
-
-    return this
-  }
-
-  setWritable (ws) {
-    if (this._writable) {
-      this._writable.removeListener('drain', this._ondrain)
-      this._writable.removeListener('finish', this._onfinish)
-      this._writable.removeListener('error', this._onerror)
-      this._writable.removeListener('close', this._onclose)
-    }
-
-    if (ws === null) {
-      this._writable = null
-      this._continueWrite(null)
-      this.end()
-      return
-    }
-
-    this._writable = ws
-    this._writable.on('drain', this._ondrain)
-    this._writable.on('finish', this._onfinish)
-    this._writable.on('error', this._onerror)
-    this._writable.on('close', this._onclose)
-
-    if (this.destroying && this._writable.destroy) {
-      this._writable.destroy()
-    }
-
-    return this
-  }
-
-  _read (cb) {
-    if (this._readable !== null) {
-      this._readable.resume()
-    }
-
-    cb(null)
-  }
-
-  _pushData (data) {
-    if (this.push(data) === false && this._readable !== null) {
-      this._readable.pause()
-    }
-  }
-
-  _pushEnd () {
-    if (this._isPipeline) {
-      this.on('end', this._decrementPipeline.bind(this))
-    }
-    this.push(null)
-    if (this._readable !== null) {
-      this._readable.removeListener('close', this._onclose)
-    }
-  }
-
-  _decrementPipeline () {
-    if (--this._pipelineMissing === 0) this._continueFinal(null)
-  }
-
-  _maybeFinal () {
-    if (this._writable !== null) {
-      this._writable.removeListener('close', this._onclose)
-    }
-
-    if (this._isPipeline) this._decrementPipeline()
-    else this._continueFinal(null)
-  }
-
-  _continueFinal (err) {
-    if (this._finalCallback === null) return
-
-    const cb = this._finalCallback
-    this._finalCallback = null
-    cb(err)
-  }
-
-  _continueWrite (err) {
-    if (this._writeCallback === null) return
-    const cb = this._writeCallback
-    this._writeCallback = null
-    cb(err)
-  }
-
-  _predestroy () {
-    if (this._writable !== null && this._writable.destroy) this._writable.destroy()
-    if (this._readable !== null && this._readable.destroy) this._readable.destroy()
-    this._continueWrite(new Error('Stream destroyed'))
-    this._continueFinal(new Error('Stream destroyed'))
-  }
-
-  _writev (datas, cb) {
-    if (this._writable === null) {
-      return cb(null)
-    }
-
-    let flushed = true
-
-    for (const data of datas) {
-      flushed = this._writable.write(data)
-    }
-
-    if (!flushed) {
-      this._writeCallback = cb
-      return
-    }
-
-    cb(null)
-  }
-
-  _final (cb) {
-    if (this._writable === null) {
-      return cb(null)
-    }
-
-    this._finalCallback = cb
-    this._writable.end()
-  }
+  this._ondata = this._pushData.bind(this);
+  this._onend = this._pushEnd.bind(this, null);
+  this._ondrain = this._continueWrite.bind(this, null);
+  this._onfinish = this._maybeFinal.bind(this);
+  this._onerror = this.destroy.bind(this);
+  this._onclose = this.destroy.bind(this, null);
 }
+
+util.inherits(Composer, stream.Duplex); // Habilitar la herencia de Duplex
+
+// Métodos estáticos
+Composer.pipeline = function() {
+  var c = new Composer();
+  c.setPipeline.apply(c, arguments);
+  return c;
+};
+
+Composer.duplexer = function(ws, rs) {
+  var c = new Composer();
+  c.setWritable(ws || null);
+  c.setReadable(rs || null);
+  return c;
+};
+
+// Definir métodos en el prototipo de Composer
+Composer.prototype.setPipeline = function(first) {
+  var streams = Array.isArray(first) ? first : Array.prototype.slice.call(arguments);
+  this._isPipeline = true;
+
+  this.setWritable(streams[0]);
+  this.setReadable(streams[streams.length - 1]);
+
+  var self = this;
+  try {
+    streams.reduce(function(prev, next) {
+      return prev.pipe(next);
+    });
+  } catch (err) {
+    self.destroy(err);
+  }
+
+  return this;
+};
+
+Composer.prototype.setReadable = function(rs) {
+  if (this._readable) {
+    this._readable.removeListener('data', this._ondata);
+    this._readable.removeListener('end', this._onend);
+    this._readable.removeListener('error', this._onerror);
+    this._readable.removeListener('close', this._onclose);
+  }
+
+  if (rs === null) {
+    this._readable = null;
+    this.push(null);
+    this.resume();
+    return this;
+  }
+
+  this._readable = rs;
+  this._readable.on('data', this._ondata);
+  this._readable.on('end', this._onend);
+  this._readable.on('error', this._onerror);
+  this._readable.on('close', this._onclose);
+
+  return this;
+};
+
+Composer.prototype.setWritable = function(ws) {
+  if (this._writable) {
+    this._writable.removeListener('drain', this._ondrain);
+    this._writable.removeListener('finish', this._onfinish);
+    this._writable.removeListener('error', this._onerror);
+    this._writable.removeListener('close', this._onclose);
+  }
+
+  if (ws === null) {
+    this._writable = null;
+    this._continueWrite(null);
+    this.end();
+    return this;
+  }
+
+  this._writable = ws;
+  this._writable.on('drain', this._ondrain);
+  this._writable.on('finish', this._onfinish);
+  this._writable.on('error', this._onerror);
+  this._writable.on('close', this._onclose);
+
+  return this;
+};
+
+Composer.prototype._read = function(cb) {
+  if (this._readable !== null) {
+    this._readable.resume();
+  }
+  cb(null);
+};
+
+Composer.prototype._pushData = function(data) {
+  if (this.push(data) === false && this._readable !== null) {
+    this._readable.pause();
+  }
+};
+
+Composer.prototype._pushEnd = function() {
+  if (this._isPipeline) {
+    this.on('end', this._decrementPipeline.bind(this));
+  }
+
+  this.push(null);
+
+  if (this._readable !== null) {
+    this._readable.removeListener('close', this._onclose);
+  }
+};
+
+Composer.prototype._decrementPipeline = function() {
+  if (--this._pipelineMissing === 0) this._continueFinal(null);
+};
+
+Composer.prototype._maybeFinal = function() {
+  if (this._writable !== null) {
+    this._writable.removeListener('close', this._onclose);
+  }
+
+  if (this._isPipeline) this._decrementPipeline();
+  else this._continueFinal(null);
+};
+
+Composer.prototype._continueFinal = function(err) {
+  if (this._finalCallback === null) return;
+  var cb = this._finalCallback;
+  this._finalCallback = null;
+  cb(err);
+};
+
+Composer.prototype._continueWrite = function(err) {
+  if (this._writeCallback === null) return;
+  var cb = this._writeCallback;
+  this._writeCallback = null;
+  cb(err);
+};
+
+// Exportar la clase Composer
+module.exports = Composer;
